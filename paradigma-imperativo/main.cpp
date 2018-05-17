bcd690f947ac583ea92fea4ba2b02972936b25df
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2018-05-17 16:55:34 -0300

    Updating functions and variables names

--- paradigma-imperativo/main.cpp
+++ paradigma-imperativo/main.cpp
@@ -5,58 +5,58 @@ using namespace std;
 using namespace std;
 
 
-void decrement_points(int points, int & user_points) {
-        user_points -= points;
+void decrement_points(int points, int & player_points) {
+        player_points -= points;
 }
 
-void win_points(int & user_points, int points) {
-        user_points += points;
+void win_points(int points, int & player_points) {
+        player_points += points;
 }
 
-void bonus_consequence(string level, int & user_points_lose, int & user_points_win) {
+void require_bonus(string level, int & player_points_lose, int & player_points_win) {
         int points;
 
         if (level == "PYTHON") {
-                points = user_points_lose * 0.1;
-                user_points_lose -= points;
-                user_points_win += points;
+                points = player_points_lose * 0.1;
+                decrement_points(points, player_points_lose);
+                win_points(points, player_points_win);
         }
         else if (level == "JAVA") {
-                points = user_points_lose * 0.15;
-                user_points_lose -= points;
-                user_points_win += points;
+                points = player_points_lose * 0.15;
+                decrement_points(points, player_points_lose);
+                win_points(points, player_points_win);
         }
         else {
-                points = user_points_lose * 0.2;
-                user_points_lose -= points;
-                user_points_win += points;
+                points = player_points_lose * 0.2;
+                decrement_points(points, player_points_lose);
+                win_points(points, player_points_win);
         }
 }
 
-void error_consequence(string level, int & user_points, int & user_life) {
+void penalize_player(string level, int & player_points, int & player_life) {
         
         if (level == "PYTHON") {
-                decrement_points(2, user_points);
+                decrement_points(2, player_points);
         }
         else if (level == "JAVA") {
-                decrement_points(5, user_points);
+                decrement_points(5, player_points);
         }
         else {
-                decrement_points(8, user_points);
+                decrement_points(8, player_points);
         }
 
-        user_life -= 1;
+        player_life -= 1;
 }
 
 
 int main() {
 
-	string user1_name;
-	string user2_name;
-	int user1_points = 8;
-	int user2_points;
-	int user1_life;
-	int user2_life;
+	string player1_name;
+	string player2_name;
+	int player1_points = 8;
+	int player2_points;
+	int player1_life;
+	int player2_life;
 	string level;
 	
 	return 0;
