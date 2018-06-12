641c1d8f85d03d730adde2ab34894ad5091588cb
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2018-06-12 20:34:51 -0300

    primeiras funções

--- paradigma-funcional/main.hs
+++ paradigma-funcional/main.hs
@@ -1,1 +1,14 @@
-modelWord:: Int -> String
+let player1 = ("", 0, 0, False, False, False, False)
+let player2 = ("", 0, 0, False, False, False, False)
+
+modelWord :: Int -> String -> String
+modelWord 0 word = word
+modelWord len word = '_' : modelWord (len-1) word
+
+verifyLetter :: [Char] -> Char -> [Char] -> [Char] 
+verifyLetter [] letter actualWord = []
+verifyLetter word letter actualWord = 
+    if (head word) == letter 
+        then [letter] ++ verifyLetter (tail word) letter actualWord
+    else 
+        [head actualWord] ++ verifyLetter (tail word) letter (tail actualWord)
