423d3093c870f97013632a210db4fde3b8be7cd8
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-06-29 15:21:59 -0300

    gets data word

--- paradigma-funcional/FileWords.hs
+++ paradigma-funcional/FileWords.hs
@@ -49,6 +49,15 @@ wordsWithLimit level
 getWord :: WordInfo -> String
 getWord returnWord = word returnWord
 
+getSynonyms :: WordInfo -> [String]
+getSynonyms returnWord = synonyms returnWord
+
+getGramaticalClass :: WordInfo -> [String]
+getGramaticalClass returnWord = gramaticalClass returnWord
+
+getSyllables :: WordInfo -> Int
+getSyllables returnWord = syllables returnWord
+
 main = do
     let wordinfo = buildWordInfo "PYTHON"
     print wordinfo
