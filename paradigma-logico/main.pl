da654fceaf53aafe2583606a851356ff3ce8fcd3
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-18 13:07:53 -0300

    add verifyLetter

--- paradigma-logico/main.pl
+++ paradigma-logico/main.pl
@@ -4,18 +4,20 @@ len([], 0).
 len([_|T], Count) :-
     len(T, Count1), Count is Count1 + 1.
 
-
 modelWord([], "").
 modelWord([_|T], Exit) :-
     modelWord(T, Under), atom_concat("_", Under, Exit).
-    
+
+verifyLetter([], [], Letter, "").
+verifyLetter([H1|T1], [H2|T2], Letter, ActualWordRet) :-
+    H2 =:= Letter, verifyLetter(T1, T2, Letter, ActualWordAtt), atom_concat(H2, ActualWordAtt, ActualWordRet);
+    verifyLetter(T1, T2, Letter, ActualWordAtt), atom_concat(H1, ActualWordAtt, ActualWordRet).
+
 
 main :-
     read_line_to_string(user_input, Word),
     atom_codes(W, Word), atom_chars(W, ListWord),
     % len(ListWord, X),
     modelWord(ListWord, Y),
-    write(Y).
-
-    
-    
+    verifyLetter(Y, ListWord, "a", ActualWord),
+    write(ActualWord).
