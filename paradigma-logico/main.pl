4b4d243dc64dc191c248751cb71338f46cdcde71
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-18 00:46:37 -0300

    mover initialization para primeira linha

--- paradigma-logico/main.pl
+++ paradigma-logico/main.pl
@@ -1,3 +1,5 @@
+:- initialization(main).
+
 len([], 0).
 len([_|T], Count) :-
     len(T, Count1), Count is Count1 + 1.
@@ -8,7 +10,6 @@ modelWord([_|T], Exit) :-
     modelWord(T, Under), atom_concat("_", Under, Exit).
     
 
-:- initialization(main).
 main :-
     read_line_to_string(user_input, Word),
     atom_codes(W, Word), atom_chars(W, ListWord),
